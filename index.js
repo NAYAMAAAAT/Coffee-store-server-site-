0ada809d1c9ef8535a761ad1874155ff12930334
NAYAMAAAAT <atefmdnayamatullah@gmail.com>
2025-02-16 22:23:26 +0600

    server-site work done & deploy

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb://localhost:27017";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,7 +25,8 @@ async function run() {
     // await client.connect();
 
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
-    const userCollection = client.db("coffeeDB").collection("user");
+    const usersCollection = client.db('insertDB').collection('users');
+
 
     // Read all data
     app.get("/coffee", async (req, res) => {
@@ -81,23 +81,47 @@ async function run() {
       res.send(result);
     });
 
-    //  User related Apis
-    app.post("/user", async (req, res) => {
-      const user = req.body;
-      console.log(user);
-      const result = await userCollection.insertOne(user);
-      res.send(result);
-    });
+        // customize to users 
 
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
+
+        app.get('/users', async (req, res) => {
+          const cursor = usersCollection.find();
+          const result = await cursor.toArray();
+          res.send(result)
+        });
+    
+    
+        app.post('/users', async (req, res) => {
+          const users = req.body;
+          const result = await usersCollection.insertOne(users);
+          res.send(result)
+        });
+    
+        app.delete('/users/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) }
+          const result = await usersCollection.deleteOne(query);
+          res.send(result)
+        });
+    
+    
+        app.patch('/users' , async (req,res)=>{
+          const email = req.body.email;
+          const filter = { email : email}
+          const updateInfo ={
+            $set : {
+              lastSignInTime : req.body.lastSignInTime
+            }
+          }
+          const result = await usersCollection.updateOne(filter , updateInfo)
+          res.send(result)
+        })
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      } finally {
+        // await client.close();
+      }
+    }
+    run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Coffee Maker server is running");
